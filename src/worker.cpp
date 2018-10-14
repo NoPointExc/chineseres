935ce3dbde6ca0ffce248c4120672527c4934410
Jiayang Sun <joeysun@fb.com>
2018-10-14 10:06:03 -0700

    Remove using namespace sdt

--- src/worker.cpp
+++ src/worker.cpp
@@ -11,11 +11,11 @@ int Worker::addTask(Task& task) {
     cout << "now tasks size is " << this->tasks.size() << endl;
     return tasks.size();
 }
- 
+
 Task& Worker::nextTask() {
     std::shared_ptr<Task> next_task = nullptr;
-	list<shared_ptr<Task>> tasks;
-    for (list<shared_ptr<Task>>::iterator it = this->tasks.begin(); it != this->tasks.end(); ++it) {
+	list<std::shared_ptr<Task>> tasks;
+    for (list<std::shared_ptr<Task>>::iterator it = this->tasks.begin(); it != this->tasks.end(); ++it) {
 	if (next_task == nullptr || next_task->getPriority() < (*it)->getPriority()) {
 	    next_task = *it;
 	}
@@ -32,38 +32,3 @@ bool Worker::work(Task& task) {
     // Release here
     return false;
 }
-
-/**
-int main(void) {
-    Task t;
-    Worker *w = new Worker("123");
-    w->work(&t);
-    cout << "Worker's name is " << w->getName() << endl;
-    w->addTask(&t);
-    for (int i=0; i<10; i++) {
-	Task *ti = new Task(20 - i);
-	w->addTask(ti);
-    }
-    for (int i=0; i<10; i++) {
-	cout << "Priority: " << w->nextTask()->getPriority() << endl;
-    }
-}
-*/
-    
-
-/**
-int main(void) {
-    Task t;
-    Worker *w = new Worker("123");
-    w->work(&t);
-    cout << "Worker's name is " << w->getName() << endl;
-    w->addTask(&t);
-    for (int i=0; i<10; i++) {
-	Task *ti = new Task(20 - i);
-	w->addTask(ti);
-    }
-    for (int i=0; i<10; i++) {
-	cout << "Priority: " << w->nextTask()->getPriority() << endl;
-    }
-}
-*/
--- src/worker.h
+++ src/worker.h
@@ -1,25 +1,21 @@
 #include <string>
 #include <list>
 #include "task.h"
-// TODO Remove std. Bad practice 
-// See also: https://stackoverflow.com/questions/1452721/why-is-using-namespace-std-considered-bad-practice
-using namespace std;
 
 class Worker {
     private:
-	const string name;
+	const std::string name;
 	const int MAX_SEARCH_LEN = 10;
 	//TODO use priority queue
-	list<shared_ptr<Task>> tasks;
+	std::list<std::shared_ptr<Task>> tasks;
     public:
 	Task& nextTask();
 	Worker(): name("default") {};
-	Worker(string name): name(name) {}; 
-	~Worker() = default;	
-	const string& getName() {return name;};
+	Worker(std::string name): name(name) {};
+	~Worker() = default;
+	const std::string& getName() {return name;};
 	bool work(Task& task);
 	// Add a new task for worker and return the current
 	// task queue size
 	int addTask(Task& task);
 };
-
